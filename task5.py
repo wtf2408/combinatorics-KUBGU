4e02ee265229f7d0245f580849ecd93777cd218d
wtf2408 <bogdanevseyv@yandex.ru>
2024-09-20 18:39:45 +0300

    init commit

---
+++ task5.py
@@ -0,0 +1,10 @@
+alf = 'abcdefghijklmnopqrstuvwxyz'
+count = 0
+for a1 in alf:
+    for a2 in alf:
+        for a3 in alf:
+            for a4 in alf:
+                for a5 in alf:
+                    count = count+1
+print(count)
+                                
